9ccffcc3f8d9b1cdb2d9411697a007b32475ca69
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-03-09 15:30:42 -0600

    update

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
+    search_fields = ('name', 'email', 'message')
+    list_filter = ('name', 'email', 'message')
+
--- contact/models.py
+++ contact/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.urls import reverse
 
 class Contact(models.Model):
+    name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone_number = models.IntegerField()
-    linkedin_url = models.CharField(max_length=50)
-    github_url = models.CharField(max_length=50)
+    message = models.TextField()
 
     def __str__(self):
-        return self.email
+        return self.email + ' - ' + self.name + ' - ' + self.message
