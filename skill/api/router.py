ecd6db95d505d04159e2bdd33d14cb08f1342c56
Antonio Queb <cardstillo@gmail.com>
2023-03-10 22:34:02 -0600

    update skill

--- skill/api/router.py
+++ skill/api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import SkillViewSet
+from skill.api.views import SkillViewSet
 
 router = DefaultRouter()
 router.register(r'skill', SkillViewSet, basename='skill')
